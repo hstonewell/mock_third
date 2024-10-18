fddd347a366f589eee5bd497b614a8d0e1426d08
haruna ishii <eijen.6@gmail.com>
2024-10-18 21:04:15 +0300

    UserProfileテーブルとモデルを追加

---
+++ src/app/Models/UserProfile.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'screen_name',
+        'postcode',
+        'address',
+        'building',
+        'image',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
