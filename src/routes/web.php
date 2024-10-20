71acf88fe92cbe5e6dad17e12bfcd9ddd8b880b7
haruna ishii <eijen.6@gmail.com>
2024-10-20 21:38:32 +0300

    ログイン・認証のルーティングを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//ログイン・認証
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
