d706500d322ca80bda8d4d54b16822b65c06aa2e
haruna ishii <eijen.6@gmail.com>
2024-10-20 21:48:22 +0300

    app.bladeとcssを追加

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECHフリマ</title>
+    <script src="https://kit.fontawesome.com/88521f16f4.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header__inner--logo">
+                ロゴ
+            </div>
+        </div>
+        @yield('header')
+    </header>
+    <main class="main">
+        @yield('main')
+    </main>
+</body>
+
+</html>
