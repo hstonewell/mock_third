df5d120c3f099f5c34387b98d45fd0ed7dc511f2
haruna ishii <eijen.6@gmail.com>
2024-10-21 23:20:18 +0300

    カラム名を修正

--- src/database/migrations/2024_10_16_092411_create_user_profiles_table.php
+++ src/database/migrations/2024_10_16_092411_create_user_profiles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('screen_name')->nullable()->unique();
+            $table->string('name')->nullable();
             $table->string('postcode')->nullable();
             $table->string('address')->nullable();
             $table->string('building')->nullable();
