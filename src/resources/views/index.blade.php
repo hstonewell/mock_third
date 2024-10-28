846a8fa68ead3710751130053ea89954b7406345
haruna ishii <eijen.6@gmail.com>
2024-10-28 23:20:14 +0200

    おすすめ商品の一覧を取得する

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -7,8 +7,8 @@
 @section('main')
 <div class="main__inner">
     <div class="main__inner__tab-wrapper">
-        <input id="tab-recommend" type="radio" name="tab_button" checked>
-        <input id="tab-mylist" type="radio" name="tab_button">
+        <input id="tab-recommend" type="radio" name="tab-button" checked>
+        <input id="tab-mylist" type="radio" name="tab-button">
         <div class="main__inner--tab-labels">
             <label class="label-recommend" for="tab-recommend">おすすめ</label>
             <label class="label-mylist" for="tab-mylist">マイリスト</label>
@@ -16,11 +16,11 @@
         <div class="main__inner__tab-content">
             <div id="tab-panel-recommend" class="tab-panel">
                 <div class="tab-panel__inner">
-                    <div class="item-thumbnail"><img src="item-thumbnail"></div>
-                    <div class="item-thumbnail"><img src="item-thumbnail"></div>
-                    <div class="item-thumbnail"><img src="item-thumbnail"></div>
-                    <div class="item-thumbnail"><img src="item-thumbnail"></div>
-                    <div class="item-thumbnail"><img src="item-thumbnail"></div>
+                    @foreach ($recommendItems as $recommendItem)
+                    <div class="tab-panel--item-thumbnail">
+                        <a href="{{ route('item.detail', ['item_id'=>$recommendItem->id]) }}"><img src="{{ asset($recommendItem->image) }}" alt="{{ $recommendItem->item_name }}"></a>
+                    </div>
+                    @endforeach
                 </div>
             </div>
             <div id="tab-panel-mylist" class="tab-panel">
