4370bfa1ec970f0a32c2b43ff30f506079e232fe
haruna ishii <eijen.6@gmail.com>
2024-10-28 23:22:14 +0200

    商品詳細ページへデータの受け渡し

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -7,15 +7,31 @@
 @section('main')
 <div class="main__inner">
     <div class="main__inner--image">
-        <img src="" class="item-image">
+        <img src="{{ asset($item->image) }}">
     </div>
     <div class="main__inner--content">
         <div class="item-header">
-            <h2>商品名</h2>
-            <span>ブランド名</span>
-            <h5>¥100,000</h5>
+            <h2>{{ $item->item_name }}</h2>
+            <span>{{ $item->brand->brand_name }}</span>
+            <h5>¥{{ number_format($item->price) }}</h5>
             <div class="item-header--buttons">
-                <i class="fa-regular fa-star fa-lg"></i>
+                @if(Auth::check())
+                @if(count($item->favorites) == 0)
+                <form method="POST" action="{{ route('favorite', ['item_id' => $item->id]) }}">
+                    @csrf
+                    <button type="submit">
+                        <i class="fa-regular fa-star fa-xl"></i>
+                    </button>
+                </form>
+                @else
+                <form method="POST" action="{{ route('unfavorite', ['item_id' => $item->id]) }}">
+                    @csrf
+                    <button type="submit">
+                        <i class="fa-solid fa-star fa-lg" style="color:#FFE500"></i>
+                    </button>
+                </form>
+                @endif
+                @endif
                 <i class="fa-regular fa-comment fa-lg"></i>
             </div>
         </div>
@@ -24,8 +40,7 @@
         </form>
         <div class="item-description">
             <h3>商品説明</h3>
-            <p>説明説明説明説明</p>
-            <p>説明説明</p>
+            <p>{{ $item->description }}</p>
         </div>
         <div class="item-information">
             <h3>商品の情報</h3>
@@ -33,19 +48,19 @@
                 <tr>
                     <th>カテゴリー</th>
                     <td class="item-category">
-                        <p>カテゴリ1</p>
-                    </td>
-                    <td class="item-category">
+                        <p>{{ $item->category->category_name }}</p>
                         <p>カテゴリ2</p>
                     </td>
                 </tr>
                 <tr>
                     <th>商品の状態</th>
-                    <td class="item-condition">状態</td>
+                    <td class="item-condition">
+                        <p>{{ $item->condition->condition }}</p>
+                    </td>
                 </tr>
             </table>
         </div>
-        <div class="item-comments__list">
+        <div class="item-comments">
             <div class="item-comment__unit">
                 <div class="item-comment-user">
                     <img src="" class="thumbnail">
