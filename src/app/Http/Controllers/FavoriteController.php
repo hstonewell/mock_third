130660bfb314be3da8b17205b866e7242517410f
haruna ishii <eijen.6@gmail.com>
2024-10-28 23:23:24 +0200

    お気に入り追加・削除機能

---
+++ src/app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Favorite;
+use Illuminate\Support\Facades\Auth;
+
+class FavoriteController extends Controller
+{
+    public function create($item_id)
+    {
+        Favorite::favorite(Auth::id(), $item_id);
+
+        return redirect()->route('item.detail', ['item_id' => $item_id]);
+    }
+
+    public function delete($item_id) {
+        Favorite::where('user_id', Auth::id())->where('item_id', $item_id)->delete();
+
+        return redirect()->route('item.detail', ['item_id' => $item_id]);
+    }
+}
