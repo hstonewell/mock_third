4519ca57b9deec0f194fd307afff9a6642d51bd6
haruna ishii <eijen.6@gmail.com>
2024-11-03 17:45:28 +0200

    住所変更ページのコントローラを追加

---
+++ src/app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserProfileRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Item;
+use App\Models\UserProfile;
+
+class UserProfileController extends Controller
+{
+    public function showAddress($item_id)
+    {
+        $item = Item::find($item_id);
+        $loginUserProfile = Auth::user()->userProfile;
+
+        return view('address', compact('item', 'loginUserProfile'));
+    }
+
+    public function createAddress(UserProfileRequest $request, $item_id)
+    {
+        $user = Auth::user();
+
+        UserProfile::updateOrCreateAddress($user->id, $request->validated());
+
+        return redirect()->route('purchase.show', ['item_id' => $item_id]);
+    }
+}
