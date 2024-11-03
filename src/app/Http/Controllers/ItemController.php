a1b1c0a066d7e8b364f06bc7da037dbaf94fda87
haruna ishii <eijen.6@gmail.com>
2024-11-03 17:53:17 +0200

    indexとdetailページそれぞれでお気に入り済商品を取得できるようにする

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Item;
 use App\Models\Comment;
+use App\Models\Favorite;
+use Illuminate\Support\Facades\Auth;
 
 class ItemController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $recommendItems = Item::recommendItems()->get();
+
+        if (Auth::check()) {
+            $userId = $request->user()->id;
+            $favoriteItems = Favorite::favoriteItems($userId)->with('item')->get();
+
+            return view('index', compact('recommendItems', 'favoriteItems'));
+        }
 
         return view('index', compact('recommendItems'));
     }
@@ -18,6 +27,8 @@ class ItemController extends Controller
     public function detail($id)
     {
         $item = Item::with('user', 'brand', 'category', 'condition')->find($id);
+
+        $favorites = Item::withCount('favorites')->get();
 
         $comments = Comment::forComment($id)->get();
 
