7e4afee1dabb8fcc7f9766528a41ea5a3ff3ecb9
haruna ishii <eijen.6@gmail.com>
2024-11-03 23:21:24 +0200

    クラス名修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,43 +5,49 @@
 @endsection
 
 @section('main')
-<div class="main__inner">
-    <div class="main__inner__tab-wrapper">
+<div class="main">
+    <div class="main__tab-wrapper">
         <input id="tab-recommend" type="radio" name="tab-button" checked>
         <input id="tab-mylist" type="radio" name="tab-button">
-        <div class="main__inner--tab-labels">
-            <label class="label-recommend" for="tab-recommend">おすすめ</label>
-            <label class="label-mylist" for="tab-mylist">マイリスト</label>
+        <div class="main__tab-labels">
+            <label class="main__tab-label tab-label--recommend" for="tab-recommend">おすすめ</label>
+            <label class="main__tab-label tab-label--mylist" for="tab-mylist">マイリスト</label>
         </div>
-        <div class="main__inner__tab-content">
-            <div id="tab-panel-recommend" class="tab-panel">
-                <div class="tab-panel__inner">
+        <div class="main__tab-content">
+            <div id="tab-panel--recommend" class="tab-content__panel">
+                <div class="tab-panel__items">
                     @foreach ($recommendItems as $recommendItem)
-                    <div class="tab-panel--item-thumbnail">
-                        <a href="{{ route('item.detail', ['item_id'=>$recommendItem->id]) }}"><img src="{{ asset($recommendItem->image) }}" alt="{{ $recommendItem->item_name }}"></a>
+                    <div class="tab-panel__item-thumbnail">
+                        <a href="{{ route('item.detail', ['item_id'=>$recommendItem->id]) }}">
+                            <img src="{{ asset($recommendItem->image) }}" alt="{{ $recommendItem->item_name }}">
+                        </a>
                     </div>
                     @endforeach
-                    <div class="spacer"></div>
-                    <div class="spacer"></div>
-                    <div class="spacer"></div>
+                    <div class="tab-panel__spacer"></div>
+                    <div class="tab-panel__spacer"></div>
+                    <div class="tab-panel__spacer"></div>
                 </div>
             </div>
-            <div id="tab-panel-mylist" class="tab-panel">
-                <div class="tab-panel__inner">
+            <div id="tab-panel--mylist" class="tab-content__panel">
+                <div class="tab-panel__items">
                     @if (Auth::check())
                     @foreach ($favoriteItems as $favoriteItem)
-                    <div class="tab-panel--item-thumbnail">
-                        <a href="{{ route('item.detail', ['item_id'=>$favoriteItem->item->id]) }}"><img src="{{ asset($favoriteItem->item->image) }}" alt="{{ $favoriteItem->item->item_name }}"></a>
+                    <div class="tab-panel__item-thumbnail">
+                        <a href="{{ route('item.detail', ['item_id'=>$favoriteItem->item->id]) }}">
+                            <img src="{{ asset($favoriteItem->item->image) }}" alt="{{ $favoriteItem->item->item_name }}">
+                        </a>
                     </div>
                     @endforeach
                     @else
-                    <div class="tab-panel__inner--not-auth">
-                        <p>マイリストに商品を登録するには<a href="/login" class="tab-panel__inner--not-auth">ログイン</a>してください</p>
+                    <div class="tab-panel__not-auth">
+                        <p class="tab-panel__not-auth-text">マイリストに商品を登録するには
+                            <a href="/login" class="tab-panel__not-auth-link">ログイン</a>してください
+                        </p>
                     </div>
                     @endif
-                    <div class="spacer"></div>
-                    <div class="spacer"></div>
-                    <div class="spacer"></div>
+                    <div class="tab-panel__spacer"></div>
+                    <div class="tab-panel__spacer"></div>
+                    <div class="tab-panel__spacer"></div>
                 </div>
             </div>
         </div>
