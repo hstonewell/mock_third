ba947d4133430ab90f6f08cd0ba9d1b0f10911e8
haruna ishii <eijen.6@gmail.com>
2024-11-04 00:30:27 +0200

    クラス名をaddress→profileへ修正し、profile.bladeでも同じフォーマットを使用できるようにする

--- src/resources/views/address.blade.php
+++ src/resources/views/address.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('/css/address.css')}}">
+<link rel="stylesheet" href="{{ asset('/css/profile.css')}}">
 @endsection
 
 @section('main')
 <div class="main__inner">
-    <div class="edit-address__header">
+    <div class="edit-profile__header">
         <h2>住所の変更</h2>
     </div>
-    <div class="edit-address__form">
+    <div class="edit-profile__form">
         <form method="post" action="{{ route('address.create', ['item_id' => $item->id]) }}">
             @csrf
-            <div class="edit-address__form--input">
+            <div class="edit-profile__form--input">
                 <label>郵便番号</label>
                 <input type="text" name="postcode" value="{{ old('postcode', Auth::user()->userProfile->postcode ?? '') }}" />
                 @if ($errors->has('postcode'))
@@ -23,7 +23,7 @@
                 @endforeach
                 @endif
             </div>
-            <div class="edit-address__form--input">
+            <div class="edit-profile__form--input">
                 <label>住所</label>
                 <input type="text" name="address" value="{{ old('address', Auth::user()->userProfile->address ?? '') }}" />
                 @if ($errors->has('address'))
@@ -34,7 +34,7 @@
                 @endforeach
                 @endif
             </div>
-            <div class="edit-address__form--input">
+            <div class="edit-profile__form--input">
                 <label>建物名</label>
                 <input type="text" name="building" value="{{ old('building', Auth::user()->userProfile->building ?? '') }}" />
                 @if ($errors->has('building'))
