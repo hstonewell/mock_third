40b266c6f97e39cb1ce273059b100a005da793ac
haruna ishii <eijen.6@gmail.com>
2024-11-07 13:18:50 +0200

    コメントのバリデーションを追加

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    public function create(Request $request) {
+    public function create(CommentRequest $request) {
 
         $comment = $request->only('item_id', 'comment');
         $comment['user_id'] = Auth::id();
