8132fc8b29dfe6b0aef32e0efc1178ad087fc137
haruna ishii <eijen.6@gmail.com>
2024-12-27 21:21:46 +0200

    不要なコロンを削除

--- src/database/migrations/2024_10_14_133042_create_items_table.php
+++ src/database/migrations/2024_10_14_133042_create_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');;
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('brand_name')->nullable();
             $table->foreignId('category_id')->nullable()->nullOnDelete();
             $table->foreignId('condition_id')->nullable()->nullOnDelete();
