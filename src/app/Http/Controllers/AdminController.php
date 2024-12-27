dae078e5f44a135f33f932c8183c42ce820d1dde
haruna ishii <eijen.6@gmail.com>
2024-12-27 20:11:01 +0200

    不要なアクションの削除

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -9,47 +9,17 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $users = User::where('id', '!=', Auth::id())->paginate(20);
         return view('/dashboard', compact('users'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy($user_id)
     {
         $user = User::findOrFail($user_id);
         $user->delete();
 
-        return redirect()->back()->with('message', 'アカウントを削除しました');
+        return redirect()->back()->with('success', 'アカウントを削除しました');
     }
 }
